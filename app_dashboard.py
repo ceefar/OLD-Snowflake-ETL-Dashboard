e7a23dce28033d6b482edae394c114511ea801fc
Ceefar <rob.farq@googlemail.com>
2022-07-06 17:43:38 +0100

    clean af metric w/ basic queries now (sql tho)

--- app_dashboard.py
+++ app_dashboard.py
@@ -1,7 +1,13 @@
 # app_dashboard.py
+
+# ---- imports ----
+# for web app 
 import streamlit as st
 import snowflake.connector
+# for date time objects
+import datetime # from datetime import datetime
 
+# ---- snowflake db setup ----
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
 @st.experimental_singleton
@@ -10,6 +16,8 @@ def init_connection():
 
 conn = init_connection()
 
+
+# ---- functions ----
 # Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=600)
@@ -18,32 +26,61 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
+# ---- main web app ----
 def run():
-    st.write("Let gooooo")
 
-    
+    # DATE SELECTER container
+    with st.container():
+        
+        currentdate = run_query("SELECT DATE(GETDATE())")
+        yesterdate = run_query("SELECT DATE(DATEADD(day,-1,GETDATE()))")
+        firstdate = run_query("SELECT current_day FROM redshift_bizinsights ORDER BY current_day ASC LIMIT 1")
+        currentdate = currentdate[0][0]
+        yesterdate = yesterdate[0][0]
+        firstdate = firstdate[0][0]
+
+        dateme = st.date_input("What Date Would You Like Info On?", datetime.date(2022, 7, 5), max_value=yesterdate, min_value=firstdate)        
+
     # METRIC container
     with st.container():
 
-  
-        st.markdown(f"""#### :calendar: Snapshot For 05/07 """)
+        st.markdown(f"""#### :calendar: Snapshot For {dateme} """)
+        st.markdown(f"**Today is {currentdate}** (data available in x hours)") # TODO - would be sooo dope, even if its by hour (obvs never seconds)
         st.write("##")
         # ---- for st.metric header widget ----
         col1, col2, col3, col4 = st.columns(4)
 
-        tempmetric = run_query("SELECT SUM(total_revenue_for_day), AVG(avg_spend_per_customer_for_day), SUM(total_customers_for_day), SUM(total_coffees_sold_for_day) FROM redshift_bizinsights WHERE current_day = '2022-07-05';")
+        tempmetric = run_query(f"SELECT SUM(total_revenue_for_day), AVG(avg_spend_per_customer_for_day), \
+                                SUM(total_customers_for_day), SUM(total_coffees_sold_for_day) FROM redshift_bizinsights WHERE current_day = '{dateme}';")
 
         print(f"{tempmetric = }")
 
-        tempmetric1 = float(tempmetric[0][0])
-        tempmetric2 = float(tempmetric[0][1])
-        tempmetric3 = tempmetric[0][2]
-        tempmetric4 = tempmetric[0][3]
+        try:
+            tempmetric1 = float(tempmetric[0][0])
+        except TypeError:
+            tempmetric1 = 0
+
+        try:
+            tempmetric2 = float(tempmetric[0][1])
+        except TypeError:
+            tempmetric2 = 0
+        
+        try:    
+            tempmetric3 = tempmetric[0][2]
+        except TypeError:
+            tempmetric3 = 0
+
+        try:
+            tempmetric4 = tempmetric[0][3]
+        except TypeError:
+            tempmetric4 = 0
+
 
         print(f"{tempmetric1 = }")
         print(f"{tempmetric2 = }")
         print(f"{tempmetric3 = }")
         print(f"{tempmetric4 = }")
+
 
         # note delta can be can be off, normal, or inverse
         col1.metric(label="Total Revenue", value=f"${tempmetric1:.2f}", delta=f"{1:.2f}", delta_color="normal")
